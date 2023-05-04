930efc040c12c2ccaae2d452c0a6fad56321127d
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-05-04 16:53:01 +0100

    now not totally horrible to look at

--- public/asset/chat.js
+++ public/asset/chat.js
@@ -16,6 +16,8 @@ let CURSOR = 0;
 let LOOP_HANDLE = null;
 const LOOP_TIMEOUT = 1000;
 
+const MEMBER_LIST = [];
+
 /**
  * Loads the current threads of the user
  */
@@ -52,8 +54,10 @@ async function thread_load(uid) {
 	{
 		const ui_users = document.getElementById("thread_members");
 		ui_users.innerHTML = null;
+		MEMBER_LIST.length = 0;
 		for (const user of r.users) {
-			const ui_user = HTMLasDOM(/* HTML */ `<span>${user.email}</span>`);
+			MEMBER_LIST.push(user);
+			const ui_user = HTMLasDOM(HTML_member(user));
 			ui_users.appendChild(ui_user);
 		}
 	}
@@ -66,8 +70,7 @@ async function thread_load(uid) {
 	{
 		const ui_chats = document.getElementById("chatbox");
 
-		let r = await (await fetch(`/chat/${uid}/message?limit=35`)).json();
-		console.log(r);
+		let r = await (await fetch(`/chat/${uid}/message?limit=20`)).json();
 		ui_chats.innerHTML = null;
 		for (const msg of r) {
 			const ui_chat = HTMLasDOM(HTML_chat(msg));
@@ -94,6 +97,9 @@ async function receiver() {
 		for (const msg of r) {
 			const ui_chat = HTMLasDOM(HTML_chat(msg));
 			ui_chats.prepend(ui_chat);
+			ui_chat.scrollIntoView({
+				behavior: "auto"
+			})
 		}
 		CURSOR = r[r.length - 1].uid;
 	}
@@ -121,11 +127,37 @@ function sender() {
 }
 
 function HTML_chat(msg) {
+	let owner = MEMBER_LIST.find(u => u.uid === msg.owner);
+	let date = new Date(msg.created).toLocaleDateString(undefined, {
+		dateStyle: "short",
+	})
+	let time = new Date(msg.created).toLocaleTimeString(undefined, {
+		timeStyle: "short"
+	})
 	return /* HTML */ `
 	<div id="m${msg.uid}" class="msg">
-		<span>${msg.uid}</span>
-		<span>${msg.owner}</span>
-		<span>${msg.content}</span>
+		<img class="pfp" src="${avatar(owner, 32)}" width="32" height="32">
+		<div>
+			<span>${owner.name ?? owner.email}</span>
+			<span>${time} ${date}</span>
+			<span>${msg.uid}</span>
+		</div>
+		<span class="c" >${msg.content}</span>
+	</div>
+	`;
+}
+
+function avatar(user, size = 32) {
+	const name = encodeURIComponent(user.name ?? user.email ?? "User");
+	return `https://ui-avatars.com/api/?name=${name}&background=random&size=${size}&format=svg"`;
+}
+
+function HTML_member(user) {
+	const name = encodeURIComponent(user.name ?? user.email ?? "User");
+	return /* HTML */ `
+	<div class="member">
+		<img src="${avatar(user, 32)}" width="32" height="32">
+		<span>${user.email}</span>
 	</div>
 	`;
 }
