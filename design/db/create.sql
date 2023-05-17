f4cbd9baf9f5820a3b8b13e88b7bbd292a648c58
TWoolhouse <thomasrwoolhouse@gmail.com>
2023-05-18 00:55:16 +0100

    better example data

--- design/db/create.sql
+++ design/db/create.sql
@@ -61,37 +61,36 @@ CREATE TABLE TaskUser (
 	CONSTRAINT TU_BindingPK PRIMARY KEY (user, task)
 );
 
-/*INSERTING DATA INTO DB FOR DATA ANALYTICS*/
+/* FAUX DATA */
 
-INSERT INTO `User` (email, name, password, role) VALUES ("louiemthomas02@gmail.com", "Louie Thomas", "SecurePassword123", 1);
+INSERT INTO `User` (email, name, password, role) VALUES ("adalovelace1@make-it-all.co.uk", "Ada Lovelace", "LaceLover01", 3);
+INSERT INTO `User` (email, name, password, role) VALUES ("bertthebuilder@make-it-all.co.uk", "Bert Smith", "BertieIsTheBest1!", 2);
+INSERT INTO `User` (email, name, password, role) VALUES ("clarajohnson01@make-it-all.co.uk", "Clara Johnson", "MeEncantaEspana2006", 2);
+INSERT INTO `User` (email, name, password, role) VALUES ("dilip-the-dodo@make-it-all.co.uk", "Dilip Schmidt", "IAmNotADodo123", 1);
+INSERT INTO `User` (email, name, password, role) VALUES ("emilysgarn1996@make-it-all.co.uk", "Emily Garn", "EmmaStone2006", 3);
+INSERT INTO `User` (email, name, password, role) VALUES ("felix@make-it-all.co.uk", "Felix the Cat", "Felixx", 3);
 
-INSERT INTO `User` (email, name, password, role) VALUES ("adalovelace1@make-it-all.com", "Ada Lovelace", "LaceLover01", 3);
 
-INSERT INTO `User` (email, name, password, role) VALUES ("bertthebuilder@make-it-all.com", "Bert Smith", "BertieIsTheBest1!", 2);
-
-INSERT INTO `User` (email, name, password, role) VALUES ("clarajohnson01@make-it-all.com", "Clara Johnson", "MeEncantaEspana2006", 2);
-
-INSERT INTO `User` (email, name, password, role) VALUES ("dilip-the-dodo@make-it-all.com", "Dilip Schmidt", "IAmNotADodo123", 1);
-
-INSERT INTO `User` (email, name, password, role) VALUES ("emilysgarn1996@make-it-all.com", "Emily Garn", "EmmaStone2006", 3);
 
 INSERT INTO `Project` (name, leader) VALUES ("Spring Cleaning", 1);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (2, "Tidy up Room", "2023-05-01 12:00:00", NULL, NULL);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (1, "Go down to the Tip", "2023-05-01 12:00:00", NULL, NULL);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (2, "Vacuum House", "2023-05-01 12:00:00", NULL, NULL);
+INSERT INTO `ProjectTask` VALUES (1,1), (2,1), (3,1);
+INSERT INTO `TaskUser` VALUES (1, 1), (3, 1), (1, 2), (2, 2), (2, 3), (3, 3), (3, 2);
 
-INSERT INTO `Project` (name, leader) VALUES ("Destroying the One Ring", 3);
+INSERT INTO `Project` (name, leader) VALUES ("Creating API Routes", 1);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (6, "Send Message", "2023-05-01 12:00:00", NULL, NULL);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (9, "Receive Message", "2023-05-01 12:00:00", NULL, NULL);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (15, "Create Thread", "2023-05-01 12:00:00", NULL, NULL);
+INSERT INTO `ProjectTask` VALUES (4,2), (5,2), (6,2);
+INSERT INTO `TaskUser` VALUES (4, 4), (5, 5), (4, 6), (5, 6), (1, 4);
 
-INSERT INTO `Task` (workerhours, name) VALUES (4368, "Walking to Mordor");
-
-INSERT INTO `Task` (workerhours, name) VALUES (192, "Attend the Council of Elrond");
-
-INSERT INTO `Task` (workerhours, name) VALUES (432, "Pass through the Mines of Moria");
-
-INSERT INTO `Task` (workerhours, name) VALUES (2, "Tidy up Room");
-
-INSERT INTO `Task` (workerhours, name) VALUES (1, "Go down to the Tip");
-
-INSERT INTO `Task` (workerhours, name) VALUES (2, "Vacuum House");
-
-INSERT INTO `ProjectTask` VALUES (1, 2), (2, 2), (3, 2);
-INSERT INTO `ProjectTask` VALUES (4, 1), (5, 1), (6, 1);
-
-INSERT INTO `TaskUser` VALUES (2,3) , (4,3) , (4,1) , (2,6) , (3,2) , (1,5) , (3,1);
+INSERT INTO `Project` (name, leader) VALUES ("Destroying the One Ring", 2);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (2, "Eating Breakfast", "2023-04-29 12:00:00", "2023-05-03 12:00:00", NULL);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (53, "Walking to Mordor", "2023-05-01 12:00:00", "2023-05-10 12:00:00", "2023-05-15 12:00:00");
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (9, "Attend the Council of Elrond", "2023-05-06 12:00:00", "2023-05-15 12:00:00", "2023-05-17 12:00:00");
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (42, "Pass through the Mines of Moria", "2023-05-10 12:00:00", "2023-05-16 12:00:00", NULL);
+INSERT INTO `Task` (workerhours, name, created, started, completed) VALUES (5, "Cast the Ring into the fire", "2023-05-18 12:00:00", NULL, NULL);
+INSERT INTO `ProjectTask` VALUES (7,3), (8,3), (9,3);
+INSERT INTO `TaskUser` VALUES (1, 7), (2, 8), (3, 9), (4, 7), (5, 8);
